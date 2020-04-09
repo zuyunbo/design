c4e324918acbcf2cd422de104aed2cfe8826ff4c
zuyunbo <12345678>
2020-04-09 23:55:51 +0800

    adapter

--- adapter/src/main/java/com/example/adapter/FishingBoatAdapter.java
+++ adapter/src/main/java/com/example/adapter/FishingBoatAdapter.java
@@ -1,6 +1,8 @@
 package com.example.adapter;
 
-public class FishingBoatAdapter implements RowingBoat{
+import java.util.*;
+
+public class FishingBoatAdapter implements RowingBoat {
 
     FishingBoat fishingBoat;
 
@@ -12,4 +14,31 @@ public class FishingBoatAdapter implements RowingBoat{
     public void row() {
         fishingBoat.sail();
     }
+
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++){
+            for (int j = i + 1; j <= n; j++){
+                if (allUnique(s, i, j)){
+                    ans = Math.max(ans, j - i);
+                }
+            }
+
+        }
+    }
+
+
+    public static boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+
 }
